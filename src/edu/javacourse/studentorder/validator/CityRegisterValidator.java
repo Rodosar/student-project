ee4432541cc6f4373fa4e236681bc26fe656774c
Ruslan Kerimov <rodosar1@mail.ru>
2021-09-18 15:32:45 +0300

    Доработал обработку исключений

--- src/edu/javacourse/studentorder/validator/CityRegisterValidator.java
+++ src/edu/javacourse/studentorder/validator/CityRegisterValidator.java
@@ -50,6 +50,10 @@ public class CityRegisterValidator {
             ex.printStackTrace(System.out);
             status = AnswerCityRegisterItem.CityStatus.ERROR;
             error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
+        } catch (Exception ex){
+            ex.printStackTrace(System.out);
+            status = AnswerCityRegisterItem.CityStatus.ERROR;
+            error = new AnswerCityRegisterItem.CityError(IN_CODE, ex.getMessage());
         }
 
         AnswerCityRegisterItem ans = new AnswerCityRegisterItem(status, person, error);
