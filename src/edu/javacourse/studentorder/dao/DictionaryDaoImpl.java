166c0db34aefbfa7872297192a4a70d3e04e7260
Ruslan Kerimov <rodosar1@mail.ru>
2021-09-27 01:10:51 +0300

    Создал конфиг файл, чтобы вытягивать поля из файла для подключения к БД

--- src/edu/javacourse/studentorder/dao/DictionaryDaoImpl.java
+++ src/edu/javacourse/studentorder/dao/DictionaryDaoImpl.java
@@ -1,5 +1,6 @@
 package edu.javacourse.studentorder.dao;
 
+import edu.javacourse.studentorder.config.Config;
 import edu.javacourse.studentorder.domain.Street;
 import edu.javacourse.studentorder.exception.DaoException;
 
@@ -14,8 +15,9 @@ public class DictionaryDaoImpl implements DictionaryDao {
 
     private Connection getConnection() throws SQLException {
         Connection connection = DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/jc_student",
-                "postgres", "QWa_172201");
+                Config.getProperty(Config.DB_URL),
+                Config.getProperty(Config.DB_LOGIN),
+                Config.getProperty(Config.DB_PASSWORD));
         return connection;
     }
 
