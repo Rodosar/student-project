8601945ddd133c418dd8b0ac1b7b4361775e45a2
Rodosar <79004996+Rodosar@users.noreply.github.com>
2022-05-28 21:33:53 +0300

    Update DictionaryDao.java

--- src/edu/javacourse/studentorder/dao/DictionaryDao.java
+++ src/edu/javacourse/studentorder/dao/DictionaryDao.java
@@ -5,15 +5,14 @@ import edu.javacourse.studentorder.domain.PassportOffice;
 import edu.javacourse.studentorder.domain.RegisterOffice;
 import edu.javacourse.studentorder.domain.Street;
 import edu.javacourse.studentorder.exception.DaoException;
+
 import java.util.List;
 
 public interface DictionaryDao {
-    List<Street> findStreets(String var1) throws DaoException;
 
-    List<PassportOffice> findPassportOffices(String var1) throws DaoException;
-
-    List<RegisterOffice> findRegisterOffices(String var1) throws DaoException;
-
-    List<CountryArea> findAreas(String var1) throws DaoException;
+    List<Street> findStreets (String pattern) throws DaoException;
+    List<PassportOffice> findPassportOffices(String areaId) throws DaoException;
+    List<RegisterOffice> findRegisterOffices(String areaId) throws DaoException;
+    List<CountryArea> findAreas(String areaId) throws DaoException;
 
 }
